d7d80e4ce5c6b417b4a3ccb262bc65efa68454c2
Tomas Tomecek <ttomecek@redhat.com>
2017-05-31 17:30:03 +0200

    better ci/cd script
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- hack/ci.sh
+++ hack/ci.sh
@@ -8,31 +8,39 @@ mkdir -p ~/.docker && echo "${DOCKER_AUTH}" >~/.docker/config.json
 
 set -x
 
+BUILD_FAILURES=0
+LAST_ACTION_PASSED=0
 
-make ci-nightly
-make ci-nightly-test
-TESTS_PASSED=$?
-if [[ $TESTS_PASSED == 0 ]] ; then
+make ci-nightly && make ci-nightly-test || LAST_ACTION_PASSED=$?
+if [[ $LAST_ACTION_PASSED == 0 ]] ; then
   VERSION=$(make get-nightly-version)
   # FIXME: move this to makefile
   docker tag tomastomecek/rust:nightly tomastomecek/rust:$VERSION
   docker push tomastomecek/rust:nightly
   docker push tomastomecek/rust:$VERSION
+else
+  BUILD_FAILURES=$((BUILD_FAILURES+1))
 fi
 
-make ci-stable
-make ci-stable-test
-TESTS_PASSED=$?
-if [[ $TESTS_PASSED == 0 ]] ; then
+LAST_ACTION_PASSED=0
+
+make ci-stable && make ci-stable-test || LAST_ACTION_PASSED=$?
+if [[ $LAST_ACTION_PASSED == 0 ]] ; then
   VERSION=$(make get-stable-version)
   docker tag tomastomecek/rust tomastomecek/rust:$VERSION
   docker push tomastomecek/rust
   docker push tomastomecek/rust:$VERSION
+else
+  BUILD_FAILURES=$((BUILD_FAILURES+1))
 fi
 
-make ci-clippy
-make ci-clippy-test
-TESTS_PASSED=$?
-if [[ $TESTS_PASSED == 0 ]] ; then
+LAST_ACTION_PASSED=0
+
+make ci-clippy && make ci-clippy-test || LAST_ACTION_PASSED=$?
+if [[ $LAST_ACTION_PASSED == 0 ]] ; then
   docker push tomastomecek/rust:clippy
+else
+  BUILD_FAILURES=$((BUILD_FAILURES+1))
 fi
+
+exit $BUILD_FAILURES
